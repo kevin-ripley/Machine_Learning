3f2e6d5c7440b751a7c6a7013e8099de99be99aa
q46p749 <q46p749@bz-cs-cslab18.msu.montana.edu>
2016-11-14 12:02:52 -0700

    Working on the missing value attributes

--- machine_learning/src/machine_learning/PreProcess.java
+++ machine_learning/src/machine_learning/PreProcess.java
@@ -7,21 +7,26 @@ package machine_learning;
 
 import java.util.ArrayList;
 
-
-
-
-
 /**
  *
  * @author Kevin Ripley
  */
 public class PreProcess {
-    Node n = new Node();
-   
-    
-    public void processIt() {
-        
-     
-       
-}
+
+    public void processIt(ArrayList<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+
+        }
+    }
+
+    //this method is for housevotes missing values
+    //1=0, 2=0. 3=12. 4=48, 5=11, 6=11, 7=15, 8=11, 9=14, 10=15, 11=22, 12=7, 13=21, 14=31, 15=25, 16=17, 17=28
+    public void hVote(ArrayList<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.contains("?")) {
+                list.get(i);
+                list.set(i, "0");
+            }
+        }
+    }
 }
