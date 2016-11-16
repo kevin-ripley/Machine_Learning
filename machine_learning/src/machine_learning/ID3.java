21348d0262570b7f92e06bd6d6dfe65ca516c67d
Bryan Downs <xerobasix@gmail.com>
2016-11-16 09:23:56 -0700

    started ID3

---
+++ machine_learning/src/machine_learning/ID3.java
@@ -0,0 +1,23 @@
+package machine_learning;
+
+import java.util.ArrayList;
+
+/**
+ * FML
+ * @author Bryan
+ */
+public class ID3 {
+    // instance variables
+    String file = "";
+    ArrayList<Node> data;
+    
+    
+    // constructor
+    public ID3 (ArrayList<Node> d, String f) {
+        this.data = d;
+        this.file = f;
+    }
+    
+    // Methods :D
+    
+}
