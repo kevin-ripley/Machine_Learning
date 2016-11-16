555ce05d94accf26e9c4f475aee1397b15c6f6d2
Bryan Downs <xerobasix@gmail.com>
2016-11-16 10:02:00 -0700

    ID3 tree-house'n

--- machine_learning/src/machine_learning/ID3.java
+++ machine_learning/src/machine_learning/ID3.java
@@ -4,20 +4,62 @@ import java.util.ArrayList;
 
 /**
  * FML
+ *
  * @author Bryan
  */
 public class ID3 {
+
     // instance variables
     String file = "";
     ArrayList<Node> data;
-    
-    
+    ArrayList<Node> test = new ArrayList<>();
+    ArrayList<Node> train = new ArrayList<>();
+
     // constructor
-    public ID3 (ArrayList<Node> d, String f) {
+    public ID3(ArrayList<Node> d, String f) {
         this.data = d;
         this.file = f;
     }
-    
+
     // Methods :D
-    
+    public void fiveXTwo() {
+        // repeat the two fold test 5 times
+        for (int i = 0; i < 5; i++) {
+            // run once
+            splitData();
+            buildTree();
+            testTree();
+            // swap sets and run again
+            switchSets();
+            buildTree();
+            testTree();
+        }
+    }
+
+    /**
+     * The splitData method splits the data into a test and training set for 5x2
+     * Fold Cross Validation
+     */
+    private void splitData() {
+        for (int i = 0; i < data.size(); i++) {
+            if (i < data.size() / 2) {
+                test.add(i, data.get(i));
+            } else {
+                train.add(i, data.get(i));
+            }
+        }
+    }
+
+    private void switchSets() {
+
+    }
+
+    private void buildTree() {
+
+    }
+
+    private void testTree() {
+
+    }
+
 }
