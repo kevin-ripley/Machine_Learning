cd1ac481673c7a9c4c8d4e2b61644069dd7f6e91
AustinBaune <austinbaune@msn.com>
2016-11-18 13:20:38 -0700

    cleaning up

--- machine_learning/src/machine_learning/Machine_learning.java
+++ machine_learning/src/machine_learning/Machine_learning.java
@@ -27,76 +27,42 @@ public class Machine_learning {
         files[3] = "soybean-small.data.txt";
         files[4] = "breast-cancer-wisconsin.data.txt";
 
-//        for (int i = 0; i < files.length; i++) {
-//            PreProcess preProcess = new PreProcess();
-//            ArrayList<Node> data = new ArrayList<>();
-//            try (FileReader process = new FileReader(files[i])) {
-//                Scanner scan = new Scanner(process);
-//                scan.useDelimiter("\n");
-//
-//                // Populate an array list of Nodes to be preprocess and given to an algorithm
-//                while (scan.hasNext() != false) {
-//                    Node node = new Node();
-//                    node.setValue(scan.next());
-//                    // node.displayNode();
-//                    data.add(node);
-//                }
-//                process.close();
-//                // Process our data/test set for file 0 and 4
-//                //preProcess.missingValues(data, files[0]);
-//                preProcess.missingValues(data, files[i]);
-//                // Do this for all files
-//                preProcess.discretize(files[i], data);
-//                preProcess.shuffle(data);
-//                preProcess.stratify(files[i], data);
-//                // preProcess.print(data);
-//                //System.out.println(data.size());
-//            }
+        for (int i = 0; i < files.length; i++) {
+            PreProcess preProcess = new PreProcess();
+            ArrayList<Node> data = new ArrayList<>();
+            try (FileReader process = new FileReader(files[i])) {
+                Scanner scan = new Scanner(process);
+                scan.useDelimiter("\n");
+
+                // Populate an array list of Nodes to be preprocess and given to an algorithm
+                while (scan.hasNext() != false) {
+                    Node node = new Node();
+                    node.setValue(scan.next());
+                    // node.displayNode();
+                    data.add(node);
+                }
+                process.close();
+                // Process our data/test set for file 0 and 4
+                //preProcess.missingValues(data, files[0]);
+                preProcess.missingValues(data, files[i]);
+                // Do this for all files
+                preProcess.discretize(files[i], data);
+                preProcess.shuffle(data);
+                preProcess.stratify(files[i], data);
+                // preProcess.print(data);
+                //System.out.println(data.size());
+            }
 //
 //            //Start the NB Algorithm
 //            NaiveBayes NB = new NaiveBayes(data, files[i]);
 //            NB.setNBData();
-//
-//        }
-
-        PreProcess preProcess = new PreProcess();
-        ArrayList<Node> data = new ArrayList<>();
-        try (FileReader process = new FileReader(files[2])) {
-            Scanner scan = new Scanner(process);
-            scan.useDelimiter("\n");
-
-            // Populate an array list of Nodes to be preprocess and given to an algorithm
-            while (scan.hasNext() != false) {
-                Node node = new Node();
-                node.setValue(scan.next());
-                data.add(node);
-            }
-            process.close();
-
-            preProcess.missingValues(data, files[2]);
-            // Do this for all files
-            preProcess.discretize(files[2], data);
-            preProcess.shuffle(data);
-            preProcess.stratify(files[2], data);
+            NB_Tan nbt = new NB_Tan(data, files[i]);
+            nbt.prepareGraph();
 
         }
+
 //        NearestNeighbor nn = new NearestNeighbor(data, files[1]);
 //        nn.setNNData();
-//        
-        NB_Tan nbt = new NB_Tan(data, files[2]);
-        nbt.prepareGraph();
-        
-        //ArrayList<Node> test;
-//        preProcess.shuffle(data);
-//        preProcess.stratify(files[1], data);
-//        for (int i = 0; i < data.size(); i++) {System.out.println("");
-//            for (int j = 0; j < data.get(i).getValue().size(); j++) {
-//                System.out.print(data.get(i).getValue().get(j));
-//                
-//            }
-//        }
 
-        //ID3 id3 = new ID3(data, files[1], preProcess.getClassList(files[0]));
-        //id3.printlist();
     }
 }
